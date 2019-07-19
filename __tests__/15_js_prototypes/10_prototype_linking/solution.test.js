af5cc0ec9a518be9a131fcbc3b1006c0e1f75637
Dmitry <dima@makovsky.me>
2019-07-19 15:47:48 +0300

    15 course: 10 prototypes linking

---
+++ __tests__/15_js_prototypes/10_prototype_linking/solution.test.js
@@ -0,0 +1,47 @@
+/* eslint-disable no-undef */
+import fs from 'fs';
+import path from 'path';
+import parse from '../../../src/15_js_prototypes/10_prototype_linking/solution';
+
+describe('HtmlBuilder', () => {
+  it('#parse', () => {
+    const data = ['html', [
+      ['head', [
+        ['title', 'hello, hexlet!'],
+      ]],
+      ['body', [
+        ['h1', { class: 'header' }, 'html builder example'],
+        ['div', [
+          ['span', 'span text'],
+          ['hr'],
+        ]],
+      ]],
+    ]];
+    const ast = parse(data);
+
+    const expectedFixturePath = path.join(__dirname, '__fixtures__', 'expected');
+    const rawData = fs.readFileSync(expectedFixturePath, 'utf-8');
+    const expected = JSON.parse(rawData);
+    expect(ast).toEqual(expected);
+  });
+
+  it('#toString', () => {
+    const data = ['html', [
+      ['head', [
+        ['title', 'hello, hexlet!'],
+      ]],
+      ['body', [
+        ['div', { class: 'separator' }],
+        ['h1', { class: 'header' }, 'html builder example'],
+        ['div', [
+          ['img', { class: 'image', href: '#' }],
+          ['span', 'span text2'],
+        ]],
+      ]],
+    ]];
+
+    const ast = parse(data);
+    const expected = '<html><head><title>hello, hexlet!</title></head><body><div class="separator"></div><h1 class="header">html builder example</h1><div><img class="image" href="#"><span>span text2</span></div></body></html>';
+    expect(ast.toString()).toEqual(expected);
+  });
+});
